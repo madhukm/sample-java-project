e5e8225525aa3b0d8012f9491b3d40d610471f66
madhukm <madhukm24@gmail.com>
2019-06-14 06:23:25 +0000

    code modified by madhu - development team

--- sample.java
+++ sample.java
@@ -3,7 +3,7 @@ class sample
 	public static void main(String[] args){
 		{
 			System.out.println("Welcome to Jenkins!!Java compilation");
-			System.out.println("Welcome to Jenkins!!Java");
+			System.out.println("Welcome to Branch");
 		}
 	}
 }
